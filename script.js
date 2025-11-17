f5d6b82fcbb92a50c76ca33811753ae9a8ac478f
Leonardo Kulkamp <kulkamp.leonardo@escola.pr.gov.br>
2025-11-17 14:01:23 +0000

    finalizado

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 const mcqueen = document.getElementById("lampada");
 
-lampada.addEventListener("mouseover",azul);
-lampada.addEventListener("mouseout",vermelho);
-
-
 function azul() {
     if (mcqueen) {
-        televisao.src = "azul.png"; 
+        mcqueen.src = "vermelho.png"; 
         document.body.style.background = "#000000ff";
     }
 }
 
 function vermelho() {
     if (mcqueen) {
-        televisao.src = "vermelho.png"; 
+        mcqueen.src = "azul.png"; 
         document.body.style.background = "#000000";
     }
 }
